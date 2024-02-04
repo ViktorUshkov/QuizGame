d7175e375c2f82d2c9821c79f4f2c4eda069187c
ViktorUshkov <mcderrigosh@yandex.ru>
2024-02-04 22:22:59 +0300

    Added 'no' case logic

--- quiz.py
+++ quiz.py
@@ -5,3 +5,11 @@
 if __name__ == '__main__':
     print("Добро пожаловать в викторину!")
     playing: str = input("Хотите ли вы сыграть? Введите 'Y' или 'N': ")
+    match playing:
+        case 'Y':
+            ...
+        case 'N':
+            print("Увидимся в следующий раз!")
+            quit()
+        case _:
+            ...
